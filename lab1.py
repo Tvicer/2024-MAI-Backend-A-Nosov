0d877fd7ec7149fc1857ca256c0f91405047ad25
Tvicer <sn2002-2011@mail.ru>
2024-05-02 02:40:38 +0300

    init commit

---
+++ lab1.py
@@ -0,0 +1,36 @@
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key):
+        if key not in self.cache:
+            return None
+        else:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+
+    def set(self, key, value):
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
+
+    def rem(self, key):
+        if key in self.cache:
+            del self.cache[key]
+
+cache = LRUCache(2)
+
+cache.set(1, 'a')
+cache.set(2, 'b')
+print(cache.get(1))
+cache.set(3, 'c')
+print(cache.get(2))
+cache.set(4, 'd')
+cache.rem(4)
+print(cache.get(4))
+
